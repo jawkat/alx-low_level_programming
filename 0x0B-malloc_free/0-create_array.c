ef2e0bf8cce0f8a29a0c1dcdae855c3138d15587
jawkat <kad.joliano@gmail.com>
2023-10-31 03:32:26 -0700

    create an array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+  * create char function - create char
+  * @ unsigned int size : the number
+  * @ char c : the caractére to print
+  *
+  * Return : nothing.
+  */
+
+  char *create_array(unsigned int size, char c)
+  {
+      char *vecteur;
+      unsigned int i;
+
+      if (size == 0)
+        return  (NULL);
+
+      vecteur = malloc(size * sizeof(char));
+      if( vecteur == NULL)
+        return (NULL);
+
+      for (i = 0 ; i < size ; i++)
+        vecteur [i]=c;
+
+        return (vecteur);
+  }
