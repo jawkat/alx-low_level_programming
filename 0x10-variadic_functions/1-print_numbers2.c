c086a61b7f563558b8141f41db9a91634e93f3f9
jawkat <kad.joliano@gmail.com>
2023-11-15 00:54:25 -0800

    renew

--- 0x10-variadic_functions/1-print_numbers2.c
+++ 0x10-variadic_functions/1-print_numbers2.c
@@ -10,16 +10,21 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list first;
+	va_list nums;
+	unsigned int index;
 
-	va_start(first, n);
-	for (int i = 0; i < n; i++)
+	va_start(nums, n);
+
+	for (index = 0; index < n; index++)
 	{
-		printf("%d", va_arg(first, int));
-		if (*separator != NULL && i != n - 1)
-			printf("%c ", *separator);
+		printf("%d", va_arg(nums, int));
+
+		if (index != (n - 1) && separator != NULL)
+			printf("%s", separator);
 	}
-	va_end(first);
+
 	printf("\n");
+
+	va_end(nums);
 }
 
