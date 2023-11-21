a2c8c17ea1b2becef40c2af385cb80014a530214
jawkat <kad.joliano@gmail.com>
2023-11-21 15:33:10 -0800

    first task

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - function that prints 
+ * all elements of a listint_t list
+ *
+ * @h: pointeur to firstt element in the list(head)
+ *
+ * Return: elements in tthe list
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t counter = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		counter++;
+		h = h->next;
+	}
+
+	return (counter);
+}
