911c7f8981d7b9e7dccd527a124e030e87e132b5
jawkat <kad.joliano@gmail.com>
2023-11-27 14:57:47 -0800

    ansewr for task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,15 +1,41 @@
-# include "main.h"
+#include "main.h"
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fp;
-	size_t wr, lc;
+	size_t wri;
+	int fd, lue;
 	char *texte;
 
+
 	if (filename == NULL)
+		return (0);
 
-	fd = open (filename, 0_RDWR);
+
+	texte = malloc(letters);
+
+	if (texte == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(texte);
+		return (0);
 
 
+	}
+
+	lue = read(fd, texte, letters);
+	if (lue < 0)
+	{
+		close(fd);
+		free(texte);
+	}
+
+	wri = write(1, texte, lue);
+	close(fd);
+	free (texte);
+
+	return (wri);
 
 }
