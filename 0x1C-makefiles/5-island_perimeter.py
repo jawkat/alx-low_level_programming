0336e7b9b6a694d72c1e8668cc18ba06ff3f757b
jawkat <kad.joliano@gmail.com>
2024-03-20 13:09:19 +0000

    task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,6 +3,7 @@
 5-island_perimeter
 """
 
+
 def island_perimeter(grid):
     """
     island_perimeter _summary_
@@ -10,16 +11,21 @@ def island_perimeter(grid):
     Args:
         grid (_type_): _description_
     """
-    size = 0
-    edges = 0
+    perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j] == 1:
-                size += 1
-                if (j > 0 and grid[i][j - 1] == 1):
-                    edges += 1
-                if (i > 0 and grid[i - 1][j] == 1):
-                    edges += 1
-    perimeter = size * 4 - edges * 2
+                """ check top """
+                if i == 0 or grid[i - 1][j] == 0:
+                    perimeter += 1
+                """Check bottom"""
+                if i == len(grid) - 1 or grid[i + 1][j] == 0:
+                    perimeter += 1
+                """Check left"""
+                if j == 0 or grid[i][j - 1] == 0:
+                    perimeter += 1
+                """Check right """
+                if j == len(grid[0]) - 1 or grid[i][j + 1] == 0:
+                    perimeter += 1
 
     return perimeter
